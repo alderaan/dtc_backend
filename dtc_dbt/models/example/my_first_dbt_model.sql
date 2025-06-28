dfff96f8a44cd5e836c27a1d8d0ac51ec21023a9
David Lucker <david.l@gojek.com>
2025-06-28 21:16:25 +0700

    testing

--- dtc_dbt/models/example/my_first_dbt_model.sql
+++ dtc_dbt/models/example/my_first_dbt_model.sql
@@ -11,9 +11,9 @@
 
 with source_data as (
 
-    select 1 as id
+    select 1 as id, "toast" as bread_type
     union all
-    select null as id
+    select null as id, "brown bread" as bread_type
 
 )
 
