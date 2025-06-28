21752598fca095833183233eb05564abff11d1b7
David Lucker <david.l@gojek.com>
2025-06-28 19:24:04 +0700

    materialize tables

---
+++ dtc_dbt/models/marts/dtc_profile_keywords.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['profile_id', 'keyword_id'],
+        schema='public'
+    )
+}}
+
+WITH source_profiles AS (
+    SELECT
+        username,
+        keyword_id
+    FROM {{ ref('google_search_instagram_profiles') }}
+),
+
+final_profiles AS (
+    SELECT
+        id as profile_id,
+        username
+    FROM {{ ref('dtc_profiles') }}
+)
+
+SELECT
+    fp.profile_id,
+    sp.keyword_id,
+    now() as created_at
+FROM
+    source_profiles sp
+JOIN
+    final_profiles fp ON sp.username = fp.username
+
+{% if is_incremental() %}
+
+  WHERE (fp.profile_id, sp.keyword_id) NOT IN (SELECT profile_id, keyword_id FROM {{ this }})
+
+{% endif %} 
---
+++ dtc_dbt/models/marts/dtc_profiles.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='username',
+        schema='public'
+    )
+}}
+
+WITH extracted_profiles AS (
+    SELECT DISTINCT
+        username
+    FROM {{ ref('google_search_instagram_profiles') }}
+)
+
+SELECT
+    username,
+    'th' as country,
+    'pending_review'::public.dtc_profile_status as status,
+    now() as created_at,
+    now() as updated_at
+FROM
+    extracted_profiles
+
+{% if is_incremental() %}
+
+  WHERE username NOT IN (SELECT username FROM {{ this }})
+
+{% endif %} 
